23909b58b0c2436690e2438b9854dc87e9475dac
kitzbergerg <gabriel.kitzberger@gmail.com>
2024-12-17 15:14:20 +0100

    use refs instead of cloning

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,7 @@ use std::io::Write;
 //             1
 // d.csv 1-2 c.csv
 
-type CsvField = Vec<u8>;
-
-fn read(file: &str) -> impl Iterator<Item = (CsvField, CsvField)> {
+fn read(file: &str) -> Vec<(Vec<u8>, Vec<u8>)> {
     ReaderBuilder::new()
         .has_headers(false)
         .from_path(file)
@@ -25,39 +23,34 @@ fn read(file: &str) -> impl Iterator<Item = (CsvField, CsvField)> {
                 result.get(1).unwrap().to_vec(),
             )
         })
+        .collect()
 }
 
-fn hash_join(
-    left: HashMap<CsvField, Vec<Vec<CsvField>>>,
-    right: HashMap<CsvField, Vec<CsvField>>,
+fn hash_join<'a>(
+    left: &HashMap<&'a [u8], Vec<Vec<&'a [u8]>>>,
+    right: &HashMap<&'a [u8], Vec<&'a [u8]>>,
     new_key: usize,
-) -> HashMap<CsvField, Vec<Vec<CsvField>>> {
-    left.iter()
-        .filter_map(|(key, left_rows)| {
-            right.get(key).map(|right_matches| {
-                // build cross product
-                left_rows.iter().flat_map(|left_row| {
-                    right_matches.iter().map(|right_element| {
-                        let mut new_row = left_row.clone();
-                        new_row.push(right_element.clone());
-                        new_row
-                    })
-                })
-            })
-        })
-        .fold(
-            HashMap::new(),
-            |mut acc: HashMap<CsvField, Vec<Vec<CsvField>>>, rows| {
-                rows.into_iter().for_each(|value| {
-                    acc.entry(value[new_key].clone()).or_default().push(value);
-                });
-                acc
-            },
-        )
+) -> HashMap<&'a [u8], Vec<Vec<&'a [u8]>>> {
+    let mut result = HashMap::new();
+    for (key, left_rows) in left {
+        if let Some(right_rows) = right.get(key) {
+            for left_row in left_rows {
+                for right_row in right_rows {
+                    let mut combined = left_row.to_vec();
+                    combined.push(right_row);
+                    result
+                        .entry(combined[new_key])
+                        .or_insert_with(Vec::new)
+                        .push(combined);
+                }
+            }
+        }
+    }
+    result
 }
 
-fn write_output<W: Write>(data: HashMap<CsvField, Vec<Vec<CsvField>>>, writer: &mut BufWriter<W>) {
-    data.into_values().for_each(|rows| {
+fn write_output<W: Write>(data: &HashMap<&[u8], Vec<Vec<&[u8]>>>, writer: &mut BufWriter<W>) {
+    data.values().for_each(|rows| {
         rows.into_iter().for_each(|v| {
             writer.write_all(&v[3]).unwrap();
             writer.write(b",").unwrap();
@@ -77,35 +70,46 @@ fn write_output<W: Write>(data: HashMap<CsvField, Vec<Vec<CsvField>>>, writer: &
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    let mut a: HashMap<CsvField, Vec<Vec<CsvField>>> = HashMap::new();
-    for (key, value) in read(&args[1]) {
-        a.entry(key.clone())
-            .or_default()
-            .push(vec![key, value.clone()]);
+
+    let mut a = HashMap::new();
+    let a_vec = read(&args[1]);
+    for (key, value) in a_vec.iter() {
+        a.entry(key.as_slice())
+            .or_insert_with(Vec::new)
+            .push(vec![key.as_slice(), value.as_slice()]);
     }
 
-    let mut b: HashMap<CsvField, Vec<CsvField>> = HashMap::new();
-    for (key, value) in read(&args[2]) {
-        b.entry(key).or_default().push(value);
+    let mut b = HashMap::new();
+    let b_vec = read(&args[2]);
+    for (key, value) in b_vec.iter() {
+        b.entry(key.as_slice())
+            .or_insert_with(Vec::new)
+            .push(value.as_slice());
     }
 
-    let mut c: HashMap<CsvField, Vec<CsvField>> = HashMap::new();
-    for (key, value) in read(&args[3]) {
-        c.entry(key).or_default().push(value);
+    let mut c = HashMap::new();
+    let c_vec = read(&args[3]);
+    for (key, value) in c_vec.iter() {
+        c.entry(key.as_slice())
+            .or_insert_with(Vec::new)
+            .push(value.as_slice());
     }
 
-    let mut d: HashMap<CsvField, Vec<CsvField>> = HashMap::new();
-    for (key, value) in read(&args[4]) {
-        d.entry(key).or_default().push(value);
+    let mut d = HashMap::new();
+    let d_vec = read(&args[4]);
+    for (key, value) in d_vec.iter() {
+        d.entry(key.as_slice())
+            .or_insert_with(Vec::new)
+            .push(value.as_slice());
     }
 
     // Perform joins
-    let map = hash_join(a, b, 0);
-    let map = hash_join(map, c, 3);
-    let map = hash_join(map, d, 0);
+    let map = hash_join(&a, &b, 0);
+    let map = hash_join(&map, &c, 3);
+    let map = hash_join(&map, &d, 0);
 
     // Write output
     let stdout = stdout();
     let mut writer = BufWriter::new(stdout.lock());
-    write_output(map, &mut writer);
+    write_output(&map, &mut writer);
 }
--- stats.md
+++ stats.md
@@ -1,54 +1,76 @@
 # Baseline
 
+```
    440,280,395,251      cycles
 
      120.942325499 seconds time elapsed
 
      109.870761000 seconds user
       10.984278000 seconds sys
-
+```
 
 # Compiler optimizations
 
+```
    429,701,405,581      cycles
 
      119.400103359 seconds time elapsed
 
      108.286886000 seconds user
       11.028146000 seconds sys
+```
 
 # join file by file
 
+```
    434,880,577,583      cycles
 
      116.514198733 seconds time elapsed
 
      107.258709000 seconds user
        9.169734000 seconds sys
+```
 
 # use byte vectors instead of strings
 
+```
    426,439,974,509      cycles
 
      114.846540260 seconds time elapsed
 
      105.708734000 seconds user
        9.062233000 seconds sys
+```
 
 # switch to hash join
 
+```
    352,024,313,086      cycles
 
       97.007805235 seconds time elapsed
 
       86.545248000 seconds user
       10.397422000 seconds sys
+```
 
 # simplify
 
+```
    329,846,796,589      cycles
 
       88.845377556 seconds time elapsed
 
       77.898300000 seconds user
       10.889052000 seconds sys
+```
+
+# use refs instead of cloning
+
+```
+   234,790,780,618      cycles
+
+      62.512477349 seconds time elapsed
+
+      54.184855000 seconds user
+       8.291234000 seconds sys
+```
