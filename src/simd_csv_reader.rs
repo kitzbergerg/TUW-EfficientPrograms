1b3c3f0ffd407307e6c6f7544a64061435d83cc3
kitzbergerg <gabriel.kitzberger@gmail.com>
2024-12-19 18:59:00 +0100

    simplify

--- src/simd_csv_reader.rs
+++ src/simd_csv_reader.rs
@@ -1,20 +1,21 @@
 use std::{
     collections::VecDeque,
-    simd::{cmp::SimdPartialEq, u8x64, Simd},
+    simd::{cmp::SimdPartialEq, u8x32, Simd},
 };
 
 use crate::CsvField;
 
-const CHUNK_SIZE: usize = 64;
+const CHUNK_SIZE: usize = 32;
 const QUEUE_CAPACITY: usize = 64;
 
 pub struct SimdCsvReader<'a> {
-    queue: VecDeque<(CsvField<'a>, CsvField<'a>)>,
+    queue: VecDeque<CsvField<'a>>,
     data: &'a [u8],
     current_pos: usize,
 
-    simd_newline: Simd<u8, 64>,
-    simd_comma: Simd<u8, 64>,
+    simd_newline: Simd<u8, CHUNK_SIZE>,
+    simd_comma: Simd<u8, CHUNK_SIZE>,
+    next_is_newline: bool,
 }
 
 impl<'a> SimdCsvReader<'a> {
@@ -25,6 +26,7 @@ impl<'a> SimdCsvReader<'a> {
             current_pos: 0,
             simd_newline: Simd::splat(b'\n'),
             simd_comma: Simd::splat(b','),
+            next_is_newline: false,
         }
     }
 
@@ -32,50 +34,43 @@ impl<'a> SimdCsvReader<'a> {
     fn find_next_delimiter(
         &self,
         start: usize,
-        splat: Simd<u8, 64>,
+        splat: Simd<u8, CHUNK_SIZE>,
         delimiter: u8,
-    ) -> Option<usize> {
+    ) -> usize {
         let remaining = &self.data[start..];
-        let chunks = remaining.chunks_exact(CHUNK_SIZE);
-        let remainder = chunks.remainder();
-
-        for (i, chunk) in chunks.enumerate() {
-            let v = u8x64::from_slice(chunk);
-            let mask = v.simd_eq(splat);
-            if !mask.all() {
-                let idx = mask.to_bitmask().trailing_zeros();
-                return Some(start + i * CHUNK_SIZE + idx as usize);
-            }
+        if remaining.len() < CHUNK_SIZE {
+            // panics if csv doesn't end in newline
+            remaining
+                .iter()
+                .position(|&x| x == delimiter)
+                .map(|pos| start + remaining.len() - remaining.len() + pos)
+                .unwrap()
+        } else {
+            // assumes that the delimiter can be found in the next CHUNK_SIZE bytes
+            // this is ok since keys are 4-22 bytes long
+            let mask = u8x32::from_slice(&remaining[..CHUNK_SIZE]).simd_eq(splat);
+            let idx = mask.to_bitmask().trailing_zeros();
+            start + idx as usize
         }
-
-        remainder
-            .iter()
-            .position(|&x| x == delimiter)
-            .map(|pos| start + remaining.len() - remainder.len() + pos)
     }
 
     #[inline(always)]
     fn process_chunk(&mut self) -> bool {
         let chunk_start = self.current_pos;
-        let Some(newline_pos) = self.find_next_delimiter(chunk_start, self.simd_newline, b'\n')
-        else {
+        if chunk_start >= self.data.len() {
             return false;
+        }
+
+        let pos = if self.next_is_newline {
+            self.find_next_delimiter(chunk_start, self.simd_newline, b'\n')
+        } else {
+            self.find_next_delimiter(chunk_start, self.simd_comma, b',')
         };
 
-        if chunk_start == newline_pos {
-            self.current_pos = newline_pos + 1;
-            return true;
-        }
-
-        if let Some(comma_pos) = self.find_next_delimiter(chunk_start, self.simd_comma, b',') {
-            if comma_pos < newline_pos {
-                let field1 = &self.data[chunk_start..comma_pos];
-                let field2 = &self.data[comma_pos + 1..newline_pos];
-                self.queue.push_back((field1, field2));
-            }
-        }
-
-        self.current_pos = newline_pos + 1;
+        let field = &self.data[chunk_start..pos];
+        self.queue.push_back(field);
+        self.current_pos = pos + 1;
+        self.next_is_newline = !self.next_is_newline;
         true
     }
 
@@ -93,7 +88,10 @@ impl<'a> Iterator for SimdCsvReader<'a> {
         if self.queue.is_empty() {
             self.fill_queue();
         }
-        self.queue.pop_front()
+        // there are always two
+        self.queue
+            .pop_front()
+            .map(|e| (e, self.queue.pop_front().unwrap()))
     }
 }
 
@@ -121,14 +119,5 @@ mod tests {
         assert_eq!(results[0], (b"field1".as_slice(), b"value1".as_slice()));
         assert_eq!(results[1], (b"field2".as_slice(), b"value2".as_slice()));
         assert_eq!(results[2], (b"field3".as_slice(), b"value3".as_slice()));
-    }
-
-    #[test]
-    fn test_empty_lines() {
-        let data = b"field1,value1\n\nfield2,value2\n";
-        let reader = data.as_slice().parse_csv();
-        let results: Vec<_> = reader.collect();
-
-        assert_eq!(results.len(), 2);
     }
 }
